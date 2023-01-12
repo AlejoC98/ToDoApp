993d4c28786ba05cfe4b6d4be0b4cddce6502df0
Alejandro Gomez <alegomezc98@icloud.com>
2023-01-12 08:04:09 -0700

    I added some styles to the page, I added two new inputs, one to create tasks and the other one to search a specifict task. I also create functions to the regular function of the app, I create a collapseMenu function for the left panel, a loadMenu to load all the list the user has created, openList to load all the tasks inside the list selected and we assigne from here the container title, openModal to use bootstrap modal feature and control it as we want

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 
-const tasks = [];
+// Tooltips Initialize
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Global vars
+const tasks = {};
+
+let listName;
+let taskEle;
 
 function collapseMenu() {
 
@@ -15,61 +23,122 @@ function collapseMenu() {
     }
 }
 
+function loadMenu() {
+    var panel = document.querySelector(".task-list");
+
+
+    for (list in tasks) {
+        var liEle = '<li onclick="openList(openList(\''+ list +'\'))"><a href="#"/>'+ list +'</li>';
+
+        panel.insertAdjacentHTML("beforeend", liEle);
+    }
+}
+
+function openList(list) {
+    if (list in tasks) {
+        listName = list;
+        document.querySelector("#listTitle").textContent = list;
+        loadTasks(list);
+        document.getElementsByClassName("right-panel")[0].style.opacity = 100;
+    }
+}
+
+function openModal() {
+    $("#addTaskListModal").modal("toggle");
+}
+
+function loadTasks(list) {
+    var taskList = document.querySelector("#taskList");
+
+    if (tasks[list].lenght > 0) {
+        console.log("sisa");
+    }
+
+}
+
+function createTaskList() {
+    var listInput = document.querySelector("#createList");
+
+    tasks[listInput.value] = [];
+
+    openModal();
+
+    loadMenu();
+
+}
+
 function deleteTask(task) {
     document.getElementById(task).remove();
 }
 
-function completedTask(task) {
-    var taskEle = document.getElementById(task);
-    if (event.target.checked) {
-        taskEle.style.textDecoration = "line-through";
-        document.getElementsByClassName("completed-task")[0].style.display = "block";
-        document.querySelector(".task-completed-list").appendChild(taskEle);
-    } else {
-        taskEle.style.textDecoration = "none";
-        document.getElementsByClassName("completed-task")[0].style.display = "none";
-        document.querySelector(".task-list").appendChild(taskEle);
-    }
+function editTask(task) {
+    taskEle = document.getElementById(task);
+    // saving current text
+    var currentTask = taskEle.querySelector(".check-box").innerText;
+    // Removing current text
+    taskEle.querySelector(".check-box").innerText = "";
+
+    var changeInput = '<input type="text" value="'+ currentTask +'" class="form-control" onkeypress="updateTask(\''+ currentTask +'\')">';
+
+    taskEle.querySelector(".check-box").insertAdjacentHTML("afterbegin", changeInput);
+
 }
 
-document.addEventListener("keypress", function(e) {
+function updateTask(current) {
+
+    var newTaskName = event.target.value;
+
+    var resetSatus = false;
+
+    switch (event.key) {
+        case "Enter":
+            for (let task of tasks[listName]) {
+                (task.name === current) ? task.name = newTaskName : task.name;
+            }
+            resetSatus = true;
+            break;
+        case "Escape":
+            newTaskName = current;
+            resetSatus = true;
+            break;
+            default:
+                break;
+            }
+            
+    if (resetSatus == true)
+        taskEle.querySelector(".check-box").innerHTML = '<input type="checkbox" onclick="completedTask(\'task-'+ taskEle.id +'\')">' + newTaskName;
     
+
+}
+
+document.getElementById("newTask").addEventListener("keypress", function(e) {
     if (e.key === "Enter" && e.target.value != "" && e.target.value != undefined) {
-
-        var taskPosition = "task-";
-
+        
         var task = {
             "name" : e.target.value,
             "status" : "active",
         }
 
-        tasks.push(task);
+        tasks[listName].push(task);
 
-        tasks.map((ele, index) => {
-            // (ele.name == task.name) ? taskPosition = taskPosition + index.toString() : taskPosition = "task-";
-            if (ele.name == task.name) {
-                taskPosition = taskPosition + index.toString();
-                ele["id"] = taskPosition;
-            } else {
-                taskPosition = "task-"
-            }
-        });
-
-        var taskElement = '<li class="list-group-item" id="'+ taskPosition +'">' +
-        '<span>' +
-        '<input type="checkbox" onclick="completedTask(\''+ taskPosition +'\')">' +
+        var taskElement = '<li class="list-group-item" id="task-'+ task.name +'">' +
+        '<div class="check-box">' +
+        '<input type="checkbox" onclick="completedTask(\'task-'+ task.name +'\')">' +
         task.name +
-        '</span>' +
-        '<button class="btn btn-delete" onclick="deleteTask(\''+ taskPosition +'\')">' +
+        '</div>' +
+        '<div class="action-btn">' +
+        '<button class="btn btn-delete" onclick="deleteTask(\'task-'+ task.name +'\')">' +
         '<i class="fa-solid fa-xmark"></i>' +
         '</button>' +
-        '<button class="btn btn-edit" onclick="editTask()">' +
+        '<button class="btn btn-edit" onclick="editTask(\'task-'+ task.name +'\')">' +
         '<i class="fa-solid fa-pencil"></i>' +
         '</button>' +
-        '</li>';
+        '</li>' +
+        '</div>';
 
         document.querySelector("#taskList").insertAdjacentHTML("afterbegin", taskElement);
 
         e.target.value = "";
+
     }
 });
