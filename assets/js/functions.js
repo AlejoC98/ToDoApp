482bcd7b772b0eb76194ae8aba1fd2e18c35f1db
Alejandro Gomez <alegomezc98@icloud.com>
2023-01-13 12:00:36 -0700

    I'm still implementing some features to the app

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,27 +1,99 @@
+function getLocalInfo() {
+    localContent = JSON.parse(localStorage.getItem("Lists"));
+}
 
-function updateLocalContent(key, value) {
-    console.log();
+function updateLocalContent() {
+    localStorage.setItem("Lists", JSON.stringify(localContent));
+}
+
+function createListEle(id, list) {
+    var element = '<li class="list-group-item" id="task-'+ id +'">' +
+        '<div class="check-box">' +
+        '<input type="checkbox" id="checkTask-'+ id +'">' +
+        id +
+        '</div>' +
+        '<div class="action-btn">' +
+        '<button class="btn btn-delete" id="deleteTask-'+ id +'">' +
+        '<i class="fa-solid fa-xmark"></i>' +
+        '</button>' +
+        '<button class="btn btn-edit" id="editTask-'+ id +'">' +
+        '<i class="fa-solid fa-pencil"></i>' +
+        '</button>' +
+        '</li>' +
+        '</div>';
+
+    document.querySelector(list).insertAdjacentHTML("afterbegin", element);
+}
+
+function createList(list) {
+    var insertItem = {};
+    insertItem[list] = [];
+    // Close Modal
+    $("#addTaskListModal").modal("toggle");
+    localContent.push(insertItem);
+    // Create Menu Item
+    createMenuItem([list]);
+    // Updating localStorage
+    updateLocalContent();
 }
 
 function createMenuItem(list) {
-    for(l in list) {
+    // Looping the content to create a li for every item on the array
+    for(l of list) {
+        // Creating html element
         var liEle = '<li id="list-'+ l +'" class="listItem"><a href="#"/>'+ l +'</li>';
-
+        // Adding li element to ul
         panel.insertAdjacentHTML("beforeend", liEle);
     }
 }
 
-function createList(list) {
-    // Create var for localStorage
-    var tasks = [...list];
-    // Close Modal
-    $("#addTaskListModal").modal("toggle");
-    // Create Menu Item
-    createMenuItem(tasks);
-    // Updating localStorage
-    updateLocalContent(tasks, []);
+function openMenuItem(element) {
+    var listName = element.target.innerText;
+    var listTask = [];
+    localContent.map((element, index) => {
+        if (listName in element)
+            if (element[listName].length > 0)
+                listTask = element[listName];
+    });
+
+    // Checking if this list already has tasks
+    if (listTask.length > 0)
+        // Here i will have a loop
+        console.log();
+
+    $("#listTitle").text(listName);
+    $(".right-panel").css("opacity", "100");
+    
+    currentList = listName;
 }
 
-function openMenuItem() {
+function getDate() {
+    return new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+}
 
+function createTask(input) {
+    // Checking if object list containd the current list
+    if (list = localContent.find((x) => Object.keys(x).includes(currentList) ))
+        // Going through Task object
+        list[currentList].map((task, index) => {
+            // Checking if task already exist and returning and error
+            if (task.name == input.value)
+                return "error";
+        });
+    // Creating task object
+    var task = {
+        "name" : input.value,
+        "status" : "active",
+        "date" : getDate()
+    }
+    // Updating localContent
+    localContent.find((t) => {
+        t[currentList].push(task);
+    });
+
+    // Creating task li element
+    createListEle(task.name, "#taskList");
+
+    // Updating localStorage
+    updateLocalContent();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,17 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 const completedListEle = document.querySelector("#completedTaskList");
 const currentTaskEle = document.querySelector("#taskList");
 const panel = document.querySelector("#task_list");
-const localContent = localStorage.getItem("listTasks");
+let localContent = Array();
+let currentList = '';
+
+if (localStorage.getItem("Lists") != null && JSON.parse(localStorage.getItem("List")) != null)
+    localContent = JSON.parse(localStorage.getItem("List"));
+else
+    localStorage.setItem("Lists", "[]");
 
 $(document).ready(function() {
+
+
     // Event for the modal
     $("#modal_btn").on("click", function() {
         $("#addTaskListModal").modal("toggle");
@@ -19,11 +27,23 @@ $(document).ready(function() {
     $("#newListForm").on("submit", function(){
         event.preventDefault();
         createList($(event.target).find("input").val());
+        getLocalInfo();
     });
 
     // Open List Tasks
     $("ul#task_list").on("click", "li", function(li) {
-        openMenuItem();
+        openMenuItem(li);
+        // getLocalInfo();
+    });
+    // Checking when the user wants to create a task
+    $("#newTaskForm").on("submit", function() {
+        event.preventDefault();
+        createTask(event.target.querySelector("input"));
+        // getLocalInfo();
+    });
+
+    $("#taskList").on("click", ".check-box input, .btn-delete, .btn-edit", function(){
+        alert("Sisaa");
     });
 
 });
